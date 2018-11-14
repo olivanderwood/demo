9ca16e6ffe6c846b7b8eafd144b7b5ee2742985a
olivanderwood <sunofdark1412@gmail.com>
2018-11-14 14:12:33 +0700

    second commit

--- demo/blog/views.py
+++ demo/blog/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render
 from .models import Post
+from django.db.models import Q
 
 # Create your views here.
 def list( request ):
-    data = {'Posts': Post.objects.all().order_by("-date")}
+    queryset_list=Post.objects.all()
+    query = request.GET.get("q")
+    if query:
+        queryset_list=queryset_list.filter(Q(title__icontains=query)) 
+
+    data = {'Posts': queryset_list.order_by("-date")}
     return render (request, 'blog/blog.html', data)
 
 def post(request, id):
     post=Post.objects.get(id=id)
     return render (request, 'blog/post.html', {'post': post})
 
-    
